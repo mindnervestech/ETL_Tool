2e5477119f14eef860ee025f71f63f159d5f35be
sanghapal <mindnervestech@gmail.com>
2018-06-08 15:03:21 +0530

    Remove duplicated directory

--- React_Starter_ERT_TOOl/src/routes.js
+++
@@ -1,6 +0,0 @@
-const routes = {
-  '/': 'Home',
-  '/dashboard': 'Dashboard',
-  '/login': 'Login'
-};
-export default routes;
--- React_Starter_ERT_TOOl/src/service/appServices.js
+++
@@ -1,9 +0,0 @@
-import servic from "react-services/defineService"
-import fetch from 'node-fetch';
-Service = service("NameService", function() {
-  return {
-    name: function() {
-      return "React + Services"
-    }      
-  };
-})
